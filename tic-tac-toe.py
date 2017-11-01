5afa27a4d7929d2638f3289b7885daf8d2a49e78
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-01 13:48:22 -0400

    Updated from Brython Server: 11/1/2017 1:48:36 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,24 @@
+#Emily Murphy
+#2017-11-1
+#tic-tac-toe.py - tic-tac-toe game
+from ggame import *
 
+black = Color(0x000000,1)
+white = Color(0xFFFFFF,1)
+
+whiteOutine = LineStyle(1, white)
+
+blackLine1 = LineStyle(1, black)
+blackLine2 = LineStyle(1, black)
+blackLine3 = LineStyle(1, black)
+blackLine4 = LineStyle(1, black)
+
+
+Sprite(blackLine1, (200,300))
+Sprite(blackLine2, (200,300))
+Sprite(blackLine3, (200,300))
+Sprite(blackLine4, (200,300))
+
+#runs the game
+if __name__ == '__main__':
+    
