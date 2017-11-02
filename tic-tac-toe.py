712a84b4ed866b6356434be9cdcb95ccba9a09ff
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-02 14:06:34 -0400

    Updated from Brython Server: 11/2/2017 2:06:42 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -29,8 +29,8 @@ blackLine1 = LineAsset(70, 100, blackOutline)
 blackLine2 = LineAsset(70, -100, blackOutline)
 
 
-Sprite(blackLine1,(500,200))
-Sprite(blackLine2,(500,300))
+Sprite(blackLine1,(800,200))
+Sprite(blackLine2,(800,300))
 Sprite(redCircle,(130, 100))
 Sprite(whiteCircle,(130, 100))
 
