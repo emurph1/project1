7dafdbfc51f3832992b667b6fd275be5810f65a3
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-02 14:11:31 -0400

    Updated from Brython Server: 11/2/2017 2:11:39 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
 
     Sprite(blackLine1,(700,30))
-    Sprite(blackLine2,(700,120))
+    Sprite(blackLine2,(700,170))
     Sprite(redCircle,(130, 100))
     Sprite(whiteCircle,(130, 100))
 
