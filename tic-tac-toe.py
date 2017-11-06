8064d23bb02619d0d886570d1c74d401ffcc57c4
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-06 14:05:13 -0500

    Updated from Brython Server: 11/6/2017 2:05:13 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -27,26 +27,10 @@ if __name__ == '__main__':
     Sprite(blackRect3, (250,0))#THE LEFT VERITCAL LINE
     Sprite(blackRect4, (550,0)) #THE RIGHT VERTICAL LINE
 
-#Creates the white boxes
-    square1 = RectangleAsset(250, 200, whiteOutline, red)
-    square2 = RectangleAsset(250, 175, whiteOutline, white)
-    square3 = RectangleAsset(250, 175, whiteOutline, white)
-    square4 = RectangleAsset(275, 200, whiteOutline, white)
-    square5 = RectangleAsset(275, 175, whiteOutline, white)
-    square6 = RectangleAsset(275, 175, whiteOutline, white)
-    square7 = RectangleAsset(250, 200, whiteOutline, white)
-    square8 = RectangleAsset(250, 175, whiteOutline, white)
-    square9 = RectangleAsset(250, 175, whiteOutline, white)
+#square variables
+    square1 = 
     
-    Sprite(square1)
-    Sprite(square2,(0,225))
-    Sprite(square3,(0,425))
-    Sprite(square4,(275,0))
-    Sprite(square5, (275,225))
-    Sprite(square6, (275,425))
-    Sprite(square7, (575,0))
-    Sprite(square8, (575,225))
-    Sprite(square9, (575,425))
+   
     
 #pieces: cirlce and 'x'
     redCircle = CircleAsset(75, whiteOutline, red)
