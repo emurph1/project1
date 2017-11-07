9c8d2635f46d56989337ef08d38d74f77c4c0d6e
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-07 14:36:14 -0500

    Updated from Brython Server: 11/7/2017 2:36:16 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -64,9 +64,9 @@ def fullBoard():
 def mouseClick(event):
     #if event.x < 250 and event.y <200:
         #print('square1')
-    if event.x < 550 and event.y <200:
+    if event.x < 550 and event.x > 250 and event.y <200:
         print('square2')
-    #if event.x < 600 and event.y <200:
+    #if event.x < 600 and event.x > 550 and event.y <200:
         #print('square3')
     #if event.x < 250 and event.y <200:
         #print('square4')
