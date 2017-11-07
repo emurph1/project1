5f7874c6c5e996ef08b6060ad69461f39e57974e
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-07 14:36:50 -0500

    Updated from Brython Server: 11/7/2017 2:36:52 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -66,7 +66,8 @@ def mouseClick(event):
         #print('square1')
     if event.x < 550 and event.x > 250 and event.y <200:
         print('square2')
-    if event.x < 600 and event.x > 550 and event.y <200:
+    if event.x < 700 and event.x > 550 and event.y <200:
+        print(event)
         print('square3')
     #if event.x < 250 and event.y <200:
         #print('square4')
