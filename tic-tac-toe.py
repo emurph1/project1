910fb9ec8658e27f1ea7dba9d45bdce4fc365454
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-07 14:42:27 -0500

    Updated from Brython Server: 11/7/2017 2:42:29 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -79,7 +79,9 @@ def mouseClick(event):
         print('square7')   
     if event.x < 550 and event.x > 250 and event.y <600 and event.y > 400:
         print('square8')
-    
+    if event.x < 700 and event.x > 550  and event.y <600 and event.y > 400:
+        print('square8')
+        
 #runs the game
 if __name__ == '__main__':
     
