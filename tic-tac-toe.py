a76aedf88922b29e63038dbdcbf9639fd9f18ca6
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-07 14:54:37 -0500

    Updated from Brython Server: 11/7/2017 2:54:39 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -72,11 +72,11 @@ def mouseClick(event):
         Sprite(blackLine1,(630,30)) 
         Sprite(blackLine2,(630,180))
     if event.x < 250 and event.y <415 and event.y >200:
-        Sprite(blackLine1,(60,200)) 
-        Sprite(blackLine2,(60,180))
+        Sprite(blackLine1,(60,240)) 
+        Sprite(blackLine2,(60,390))
     if event.x < 550 and event.x > 250 and event.y < 415 and event.y > 200:
-        Sprite(blackLine1,(60,30)) 
-        Sprite(blackLine2,(60,180))
+        Sprite(blackLine1,(350,240)) 
+        Sprite(blackLine2,(350,390))
     if event.x < 800 and event.x > 550 and event.y < 415 and event.y >200:
         Sprite(blackLine1,(60,30)) 
         Sprite(blackLine2,(60,180))
