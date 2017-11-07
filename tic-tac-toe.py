b76d258e09341d0ed18e0c6d79122c8e05ab2c09
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-07 14:37:58 -0500

    Updated from Brython Server: 11/7/2017 2:38:00 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -62,15 +62,15 @@ def fullBoard():
 
 #click that sprites X when it is player turn
 def mouseClick(event):
-    #if event.x < 250 and event.y <200:
+    if event.x < 250 and event.y <200:
         #print('square1')
     if event.x < 550 and event.x > 250 and event.y <200:
         print('square2')
     if event.x < 700 and event.x > 550 and event.y <200:
-        print(event.x,event.y)
         print('square3')
-    #if event.x < 250 and event.y <200:
-        #print('square4')
+    if event.x < 250 and event.y <200:
+        
+        print('square4')
     
 #runs the game
 if __name__ == '__main__':
