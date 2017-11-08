8a4177757ebe3021ba01492590ddef94c80096c3
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-08 13:26:50 -0500

    Updated from Brython Server: 11/8/2017 1:26:55 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -63,7 +63,8 @@ def fullBoard():
 def computerTurn():
     num = randint(1,9)
     if num == 1:
-        Sprite(redCircle)
+        data['square1'] = Sprite(redCircle)
+        
 
 #click that sprites X when it is player turn
 def mouseClick(event):
