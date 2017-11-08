d9c13caf01882fe447784c8ed9566430a482c9ea
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-08 16:25:05 -0500

    Updated from Brython Server: 11/8/2017 4:25:05 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -104,86 +104,86 @@ def computerTurn():
         if data['square1'] != 'X' and isEmpty(1):
             Sprite(redCircle,(130,100))
             Sprite(whiteCircle,(130,100))
-            data['square1'] += 'O'
+            data['square1'] == 'O'
     if num == 2:
         if data['square2'] != 'X' and isEmpty(2):
             Sprite(redCircle,(410,100))
             Sprite(whiteCircle,(410,100))
-            data['square2'] += 'O'
+            data['square2'] == 'O'
     if num == 3:
         if data['square3'] != 'X' and  isEmpty(3):
             Sprite(redCircle,(700,100))
             Sprite(whiteCircle,(700,100))
-            data['square3'] += 'O'
+            data['square3'] == 'O'
     if num == 4:
         if data['square4'] != 'X' and isEmpty(4):
             Sprite(redCircle,(130,310))
             Sprite(whiteCircle,(130,310))
-            data['square4'] += 'O'
+            data['square4'] == 'O'
     if num == 5:
         if data['square5'] != 'X' and isEmpty(5):
             Sprite(redCircle,(410,310))
             Sprite(whiteCircle,(410,310))
-            data['square5'] += 'O'
+            data['square5'] == 'O'
     if num == 6:
         if data['square6'] != 'X' and isEmpty(6):
             Sprite(redCircle,(700,310))
             Sprite(whiteCircle,(700,310))
-            data['square6'] += 'O'
+            data['square6'] == 'O'
     if num == 7:
         if data['square7'] != 'X' and isEmpty(7):
             Sprite(redCircle,(130,510))
             Sprite(whiteCircle,(130,510))
-            data['square7'] += 'O'
+            data['square7'] == 'O'
     if num == 8:
         if data['square8'] != 'X' and isEmpty(8):
             Sprite(redCircle,(410,510))
             Sprite(whiteCircle,(410,510))
-            data['square8'] += 'O'
+            data['square8'] == 'O'
     if num == 9:
         if data['square9'] != 'X' and isEmpty(9):
             Sprite(redCircle,(700,510))
             Sprite(whiteCircle,(700,510))
-            data['square9'] += 'O'
+            data['square9'] == 'O'
         
 #click that sprites X when it is player turn
 def mouseClick(event):
     if event.x < 250 and event.y <200 and isEmpty(1): #square1
         Sprite(blueLine1,(70,30)) 
         Sprite(blueLine2,(70,180))
-        data['square1'] += 'X'
+        data['square1'] == 'X'
     elif event.x < 550 and event.x > 250 and event.y <200 and isEmpty(2): #square2
         Sprite(blueLine1,(350,20)) 
         Sprite(blueLine2,(350,170))
-        data['square2'] += 'X'
+        data['square2'] == 'X'
     elif event.x < 800 and event.x > 550 and event.y < 200 and isEmpty(3):#square3
         Sprite(blueLine1,(630,30)) 
         Sprite(blueLine2,(630,180))
-        data['square3'] += 'X'
+        data['square3'] == 'X'
     elif event.x < 250 and event.y <415 and event.y >200 and isEmpty(4): #square4
         Sprite(blueLine1,(70,240)) 
         Sprite(blueLine2,(70,390))
-        data['square4'] += 'X'
+        data['square4'] == 'X'
     elif event.x < 550 and event.x > 250 and event.y < 415 and event.y > 200 and isEmpty(5): #square5
         Sprite(blueLine1,(350,240)) 
         Sprite(blueLine2,(350,390))
-        data['square5'] += 'X'
+        data['square5'] == 'X'
     elif event.x < 800 and event.x > 550 and event.y < 415 and event.y >200 and isEmpty(6): #square6
         Sprite(blueLine1,(630,240)) 
         Sprite(blueLine2,(630,390))
-        data['square6'] += 'X'
+        data['square6'] == 'X'
     elif event.x < 250 and event.y <600 and event.y > 415 and isEmpty(7): #square7
         Sprite(blueLine1,(70,440)) 
         Sprite(blueLine2,(70,590))
-        data['square7'] += 'X'
+        data['square7'] == 'X'
     elif event.x < 550 and event.x > 250 and event.y <600 and event.y > 415 and isEmpty(8): #square8
         Sprite(blueLine1,(350,440)) 
         Sprite(blueLine2,(350,590))
-        data['square8'] += 'X'
+        data['square8'] == 'X'
     elif event.x < 800 and event.x > 550  and event.y <600 and event.y > 415 and isEmpty(9): #square9
         Sprite(blueLine1,(630,440)) 
         Sprite(blueLine2,(630,590))
-        data['square9'] += 'X'
+        data['square9'] == 'X'
     computerTurn()
         
 #runs the game
