bd28e68ab6831a597b0ea8e6b8cc8a81cfebb6c2
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-08 15:54:23 -0500

    Updated from Brython Server: 11/8/2017 3:54:23 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -18,35 +18,35 @@ def isEmpty(squares):
         return False
     else: 
         return True
-    if data['square2'] == 'O' or data['square2'] =='X':
+    if squares == 2 and (data['square2'] == 'O' or data['square2'] =='X'):
         return False
     else: 
         return True 
-    if data['square3'] == 'O' or data['square3'] == 'X':
+    if squares == 3 and (data['square3'] == 'O' or data['square3'] == 'X'):
         return False
     else: 
         return True
-    if data['square4'] == 'O' or data['square4'] == 'X':
+    if squares == 4 and (data['square4'] == 'O' or data['square4'] == 'X'):
         return False
     else: 
         return True
-    if data['square5'] == 'O' or data['square5'] == 'X':
+    if squares == 5 and (data['square5'] == 'O' or data['square5'] == 'X'):
         return False
     else: 
         return True
-    if data['square6'] == 'O' or data['square6'] == 'X':
+    if squares == 6 and (data['square6'] == 'O' or data['square6'] == 'X'):
         return False
     else: 
         return True
-    if data['square7'] == 'O' or data['square7'] == 'X':
+    if squares == 7 and (data['square7'] == 'O' or data['square7'] == 'X'):
         return False
     else: 
         return True
-    if data['square8'] == 'O' or data['square8'] == 'X':
+    if squares == 8 and (data['square8'] == 'O' or data['square8'] == 'X'):
         return False
     else: 
         return True
-    if data['square9'] == 'O' or data['square9'] == 'X':
+    if squares == 9 and (data['square9'] == 'O' or data['square9'] == 'X'):
         return False
     else: 
         return True
@@ -135,35 +135,35 @@ def mouseClick(event):
         Sprite(blueLine1,(70,30)) 
         Sprite(blueLine2,(70,180))
         data['square1'] += 'X'
-    elif event.x < 550 and event.x > 250 and event.y <200: #square2
+    elif event.x < 550 and event.x > 250 and event.y <200 and isEmpty(2): #square2
         Sprite(blueLine1,(350,20)) 
         Sprite(blueLine2,(350,170))
         data['square2'] += 'X'
-    elif event.x < 800 and event.x > 550 and event.y < 200:#square3
+    elif event.x < 800 and event.x > 550 and event.y < 200 and isEmpty(3):#square3
         Sprite(blueLine1,(630,30)) 
         Sprite(blueLine2,(630,180))
         data['square3'] += 'X'
-    elif event.x < 250 and event.y <415 and event.y >200: #square4
+    elif event.x < 250 and event.y <415 and event.y >200 and isEmpty(4): #square4
         Sprite(blueLine1,(70,240)) 
         Sprite(blueLine2,(70,390))
         data['square4'] += 'X'
-    elif event.x < 550 and event.x > 250 and event.y < 415 and event.y > 200: #square5
+    elif event.x < 550 and event.x > 250 and event.y < 415 and event.y > 200 and isEmpty(5): #square5
         Sprite(blueLine1,(350,240)) 
         Sprite(blueLine2,(350,390))
         data['square5'] += 'X'
-    elif event.x < 800 and event.x > 550 and event.y < 415 and event.y >200: #square6
+    elif event.x < 800 and event.x > 550 and event.y < 415 and event.y >200 and isEmpty(6): #square6
         Sprite(blueLine1,(630,240)) 
         Sprite(blueLine2,(630,390))
         data['square6'] += 'X'
-    elif event.x < 250 and event.y <600 and event.y > 415: #square7
+    elif event.x < 250 and event.y <600 and event.y > 415 and isEmpty(7): #square7
         Sprite(blueLine1,(70,440)) 
         Sprite(blueLine2,(70,590))
         data['square7'] += 'X'
-    elif event.x < 550 and event.x > 250 and event.y <600 and event.y > 415: #square8
+    elif event.x < 550 and event.x > 250 and event.y <600 and event.y > 415 and isEmpty(8): #square8
         Sprite(blueLine1,(350,440)) 
         Sprite(blueLine2,(350,590))
         data['square8'] += 'X'
-    elif event.x < 800 and event.x > 550  and event.y <600 and event.y > 415: #square9
+    elif event.x < 800 and event.x > 550  and event.y <600 and event.y > 415 and isEmpty(9): #square9
         Sprite(blueLine1,(630,440)) 
         Sprite(blueLine2,(630,590))
         data['square9'] += 'X'
