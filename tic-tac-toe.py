1d4d59011a5ff7c5124892f5509b845fe92e16f4
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-11-09 14:45:18 -0500

    Updated from Brython Server: 11/9/2017 2:45:29 PM

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -96,7 +96,7 @@ def winner():
             Sprite(cWinner, (300,100))
         elif data['square2'] == 'O' and data['square5'] == 'O' and data['square8'] == 'O':
             Sprite(cWinner, (300,100))
-        if:
+        else:
             Sprite(nWinner, (300,100))
 
 #computer turn
